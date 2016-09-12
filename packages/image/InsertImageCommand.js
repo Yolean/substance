2f579ca9bb44bd773eef49c521a77b366a87e578
Johan Olofsson <johaolof@gmail.com>
2016-09-12 16:32:44 +0200

    Crude PDF inserting capability added to what is actually the InsertImage function.

--- packages/image/InsertImageCommand.js
+++ packages/image/InsertImageCommand.js
@@ -9,6 +9,11 @@ function ImageCommand() {
 
 ImageCommand.Prototype = function() {
 
+  this.getUrlPath = function() {
+    var propPath = this.constructor.urlPropertyPath;
+    return [this.props.node.id].concat(propPath);
+  };
+
   this.getCommandState = function(props, context) {
     var documentSession = context.documentSession;
     var sel = props.selection || documentSession.getSelection();
@@ -49,54 +54,102 @@ ImageCommand.Prototype = function() {
     var snippet = doc.createSnippet();
 
     // as file upload takes longer we will insert stub images
+    var path = sel.path;
     var items = files.map(function(file) {
-      var node = snippet.create({ type: 'image' });
-      snippet.show(node);
-      return {
-        file: file,
-        nodeId: node.id
-      };
-    });
+      var type = "link";
+      var node = undefined;
+      if(/\.PDF/i.test(file.name)) {
+        type = "link";
+        var textlength = file.name.length;
+        //node = snippet.create({ type: "link", title: file.name, path: path, startOffset: sel.startOffset, endOffset: (sel.startOffset+textlength) });
+        //node = snippet.create({ type: "text" });
+        sel.endOffset = sel.endOffset + textlength;
+        documentSession.setSelection(sel);
+        surface.transaction(function(tx, args) {
+          args.text = file.name;
+          surface.paste(tx, args);
+          //snippet.show(node); // without this, no node.id.
 
-    surface.transaction(function(tx) {
-      tx.before.selection = sel;
-      return paste(tx, {
-        selection: sel,
-        containerId: surface.getContainerId(),
-        doc: snippet
-      });
-    });
+          /*tx.before.selection = sel;
+          paste(tx, {
+            selection: sel,
+            containerId: surface.getContainerId(),
+            doc: snippet
+          });*/
+        });
 
-    // start uploading
-    items.forEach(function(item) {
-      var nodeId = item.nodeId;
-      var file = item.file;
-      var node = doc.get(nodeId);
-      node.emit('upload:started');
-      var channel = fileClient.uploadFile(file, function(err, url) {
-        if (err) {
-          url = "error";
-        }
-        // get the node again to make sure it still exists
+        var props = { mode: "create" };
+        var commandManager = surface._getContext().commandManager;
+        var info = commandManager.executeCommand("link", props);
+
+        var nodeId = info.anno.id;
         var node = doc.get(nodeId);
-        if (node) {
-          node.emit('upload:finished');
-          documentSession.transaction(function(tx) {
-            tx.set([nodeId, 'src'], url);
+
+        node.emit('upload:started');
+        var channel = fileClient.uploadFile(file, function(err, url) {
+          if (err) {
+            url = "error";
+          }
+          // get the node again to make sure it still exists
+          node = doc.get(nodeId);
+          if (node) {
+            node.emit('upload:finished');
+            console.log("upload:finished for PDF insert.");
+            documentSession.transaction(function(tx) {
+              console.log("Setting url to " + url + " for " + nodeId);
+              tx.set([nodeId, 'url'], url);
+            });
+          }
+        });
+        channel.on('progress', function(progress) {
+          // console.log('Progress', progress);
+          node.emit('upload:progress', progress);
+        });
+        
+        return {
+          status: 'file-upload-process-started'
+        };
+      } else if(/\.PNG/i.test(file.name) || /\.JPG/i.test(file.name)) {
+        type = "image";
+        node = snippet.create({ type: "image" });
+        snippet.show(node);
+
+        surface.transaction(function(tx) {
+          tx.before.selection = sel;
+          return paste(tx, {
+            selection: sel,
+            containerId: surface.getContainerId(),
+            doc: snippet
           });
-        }
-      });
-      channel.on('progress', function(progress) {
-        // console.log('Progress', progress);
-        node.emit('upload:progress', progress);
-      });
+        });
+
+        var nodeId = node.id;
+        var node = doc.get(nodeId);
+        node.emit('upload:started');
+        var channel = fileClient.uploadFile(file, function(err, url) {
+          if (err) {
+            url = "error";
+          }
+          // get the node again to make sure it still exists
+          var node = doc.get(nodeId);
+          if (node) {
+            node.emit('upload:finished');
+            documentSession.transaction(function(tx) {
+              tx.set([nodeId, 'src'], url);
+            });
+          }
+        });
+        channel.on('progress', function(progress) {
+          // console.log('Progress', progress);
+          node.emit('upload:progress', progress);
+        });
+
+        return {
+          status: 'file-upload-process-started'
+        };
+      }
     });
-
-    return {
-      status: 'file-upload-process-started'
-    };
-  };
-
+  }
 };
 
 Command.extend(ImageCommand);
